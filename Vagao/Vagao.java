7690879c4c975322b4e40982fdf6fa992046baa7
Eduardo Moraes <edumoraes1@icloud.com>
2026-04-10 11:43:15 -0300

    Update Vagao.java

--- Vagao/Vagao.java
+++ Vagao/Vagao.java
@@ -1,1 +1,29 @@
 
+  public class Pilha {
+    Node topo;
+
+    public Pilha() {
+        topo = null;
+    }
+
+    public void push(String vagao) {
+        Node novo = new Node(vagao);
+        novo.proximo = topo;
+        topo = novo;
+    }
+
+    public String pop() {
+        if (topo == null) {
+            return "Pilha vazia";
+        }
+        String valor = topo.vagao;
+        topo = topo.proximo;
+        return valor;
+    }
+
+    public void mostrarSaida() {
+        while (topo != null) {
+            System.out.println(pop());
+        }
+    }
+}
